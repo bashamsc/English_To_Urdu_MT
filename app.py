fbcb62f795eb0f8828b4a0b20e5b08b2bb929414
Mahaboob Basha <msc.mahaboob@gmail.com>
2021-06-21 22:50:20 +0530

    New Version

---
+++ app.py
@@ -0,0 +1,71 @@
+#importing libraries
+
+import io
+import os
+
+
+import numpy as np
+
+import json
+
+import flask
+
+import xlrd
+
+import pandas as pd
+
+from flask import Flask, render_template, flash, redirect, request, session, logging, url_for, jsonify, make_response
+
+from flask_restful import Api, Resource
+
+from flask_cors import CORS
+
+import joblib
+
+<<<<<<< HEAD
+=======
+import transformers
+
+from transformers import MarianTokenizer, MarianMTModel
+
+import torch
+
+>>>>>>> a78cfbe... Removed pkl file as it is increasing max size
+import pdb
+
+app = Flask(__name__)
+
+api = Api(app)
+
+Cors = CORS(app)
+
+
+app.config['SECRET_KEY'] = '!9m@S-dThyIlW[pHQbN^'
+
+#model = joblib.load('./models/MarianMTModel.pkl')
+
+@app.route('/')
+def home():
+
+    return render_template('index.html')
+
+
+@app.route('/translate-text', methods=['POST'])
+def translate_text():
+    
+    data = request.get_json()
+    pdb.set_trace()
+    text_input = data['text']
+    model_name = 'Helsinki-NLP/opus-mt-en-ur'
+    tokenizer = MarianTokenizer.from_pretrained(model_name)
+    model = MarianMTModel.from_pretrained(model_name)
+    translated = model.generate(**tokenizer.prepare_seq2seq_batch(text_input, return_tensors="pt"))
+    tgt_text = [tokenizer.decode(t, skip_special_tokens=True) for t in translated]
+    #pdb.set_trace()
+    return jsonify(tgt_text)
+   
+
+if __name__ == '__main__':
+    
+   
+    app.run(debug=False)
---
+++ static/scripts/main.js
@@ -0,0 +1,33 @@
+//Initiate jQuery on load.
+$(function() {
+  //Translate text with flask route
+  $("#translate").on("click", function(e) {
+    e.preventDefault();
+    var translateVal = document.getElementById("text-to-translate").value;
+    var translateRequest = { 'text': translateVal}
+
+    if (translateVal !== "") {
+      $.ajax({
+        url: '/translate-text',
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        dataType: 'json',
+        data: JSON.stringify(translateRequest),
+        success: function(data) {
+          for (var i = 0; i < data.length; i++) {
+			  if(i>0)
+			  {
+				  document.getElementById("translation-result").value += "\\n" + data[i];
+			  } 
+			  else{
+				  document.getElementById("translation-result").value = data[i];
+			  }
+		  }
+        }
+      });
+    };
+  });
+  
+})
